aab9c3961b71aebae23b7066d538edc9ee1952c3
xjhyyds <1289264578@qq.com>
2024-04-08 16:04:24 +0200

    rename

--- Lecture.py
+++ Lecture.py
@@ -2,6 +2,10 @@ import os
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 from IPython.display import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.metrics import homogeneity_completeness_v_measure
 
 
 def read_csv(base_dir):
@@ -63,6 +67,21 @@ def encoder(df, features):
     return df_tmp
 
 
+def plot_heatmap(df, column1, column2, colormap="Blues"):
+    crosstab = pd.crosstab(df[column1], df[column2])
+
+    sns.set_theme(style="whitegrid", font_scale=1)
+    plt.figure(figsize=(15, 10))
+
+    sns.heatmap(crosstab, annot=True, fmt="d", cmap=colormap)
+
+    plt.title(f"Heatmap of {column1} vs {column2}")
+    plt.xlabel(column2)
+    plt.ylabel(column1)
+
+    plt.show()
+
+
 if __name__ == "__main__":
     base_lecture_dir = "dataset\\MAC_derand_lecture-dataset"
     base_challenge_dir = "dataset\\MAC_derand_challenge"
@@ -70,11 +89,41 @@ if __name__ == "__main__":
     display(combine_df)
     clean_df = remove_NaN(df=combine_df)
     display(clean_df)
-    features_less_20 = find_features_less_20(df=clean_df)
-    encoded_df = encoder(df=clean_df, features=features_less_20)
+    features = find_features_less_20(df=clean_df)
+    encoded_df = encoder(df=clean_df, features=features)
     burst_df = encoded_df.drop(['Timestamp'], axis=1)
     burst_df = burst_df.groupby(['MAC Address'])
     burst_df = burst_df.first().reset_index()
     display(burst_df)
     label_count = burst_df["Label"].value_counts()
     print(label_count)
+
+    plot_heatmap(encoded_df, 'Label', "Supported Rates")
+    plot_heatmap(encoded_df, 'Label', "Extended Supported Rates")
+    plot_heatmap(encoded_df, 'Label', "Vendor Specific Tags")
+    plot_heatmap(encoded_df, 'Label', "HT Capabilities")
+    plot_heatmap(encoded_df, 'Label', "Extended Capabilities")
+
+    columns_to_keep = features
+    features.insert(0, "MAC Address")
+    features.append("Label")
+
+    cluster_df = burst_df[features].copy().reset_index()
+    print(f"features:{features}")
+
+    selected_features = ["HT Capabilities", "Extended Capabilities"]
+    cluster_df["Cluster ID"] = cluster_df.groupby(selected_features).ngroup()
+
+    display(cluster_df)
+
+    plot_heatmap(cluster_df, 'Label', "Cluster ID")
+
+    n_unique_clusterid = len(np.unique(cluster_df["Cluster ID"]))
+    n_unique_label = len(np.unique(cluster_df["Label"]))
+    print("Error", n_unique_clusterid - n_unique_label)
+
+    h, c, v = homogeneity_completeness_v_measure(cluster_df["Label"], cluster_df["Cluster ID"])
+
+    print("Homog: ", h)
+    print("Completeness: ", c)
+    print("V-Meas: ", v)
